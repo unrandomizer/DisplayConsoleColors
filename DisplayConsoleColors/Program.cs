f916cabc9395c09f3f52d81453ab8a89ff66d153
Rostislav <therostikru@gmail.com>
2020-08-09 04:39:48 +0300

    Add project files.

---
+++ DisplayConsoleColors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DisplayConsoleColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (ConsoleColor color in Enum.GetValues(typeof(ConsoleColor)))
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = color;
+                Console.Write($"\tSweety Black on {color} background");
+                Console.ResetColor();
+                Console.WriteLine("\n");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = color;
+                Console.Write($"\tSweety White on {color} background");
+                Console.ResetColor();
+                Console.WriteLine("\n");
+
+                Console.ForegroundColor = color;
+                Console.Write($"\tSweety {color}");
+                Console.ResetColor();
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
